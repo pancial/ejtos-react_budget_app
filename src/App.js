2f5abe53cd48d74f7c025de7259090c1b5185c43
pancial <pan930728@gmail.com>
2024-02-21 01:36:41 -0500

    Assignment Update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
+import Currency from './components/Currency';
 import AllocationForm from './components/AllocationForm';
 
 import { AppProvider } from './context/AppContext';
@@ -18,7 +18,7 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
+                    <div className='row mt-4'>
                         {
                             /* Add Budget component here */
                             // Budget component
@@ -41,8 +41,15 @@ const App = () => {
                             <div className='col-sm'>
                                 <ExpenseTotal />
                             </div>
-                        }        
-                       
+                        } 
+
+                        {
+                            <div className='col-sm'>
+                                <Currency />
+                            </div>
+                        }       
+
+                    <h3 className='mt-3'>Allocation</h3>   
                         {
                             /* Add ExpenseList component here */
                             <div className='col-sm'>
@@ -50,13 +57,7 @@ const App = () => {
                             </div>
                         }         
 
-                        {
-                            /* Add ExpenseItem component here */
-                            <div className='col-sm'>
-                                <ExpenseItem />
-                            </div>
-                        }        
-
+                    <h3 className='mt-3'>Change allocation</h3>  
                         {
                             /* Add AllocationForm component here under */
                             <div className='col-sm'>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,33 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     const handleBudgetChange = (event) => {
+        if(event.target.value > 20000) {
+            alert("The value cannot exceed remaining funds  £20000");
+            return;
+        }
+
+        if(event.target.value < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+
         setNewBudget(event.target.value);
+        
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        });
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
